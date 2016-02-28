fbab2845a3e9c3e7e7fdff424c74b07f5102dd4e
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-02-28 14:28:15 +0100

    devnull not permitted

--- lnxsvc98d.py
+++ lnxsvc98d.py
@@ -91,13 +91,13 @@ def do_mv_data(rpath):
     if os.path.isfile(clientlock) and not (os.path.isfile(rpath + "/" + os.path.split(fname)[1])):
       syslog_trace("...moving data {0}".format(fname), False, DEBUG)
       #shutil.move(fname, rpath)
-      shutil.move(fname, "/dev/null")
+      shutil.move(fname, fname+".DEAD")
 
   for fname in glob.glob(r'/tmp/' + leaf + '/*.png'):
     if os.path.isfile(clientlock) and not (os.path.isfile(rpath + "/" + os.path.split(fname)[1])):
       syslog_trace("...moving graph {0}".format(fname), False, DEBUG)
       #shutil.move(fname, rpath)
-      shutil.move(fname, "/dev/null")
+      shutil.move(fname, fname+".DEAD")
 
   unlock(clientlock)
   syslog_trace("!..UNLOCK", False, DEBUG)
