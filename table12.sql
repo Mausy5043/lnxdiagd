934bd156a522e8d0de1e16f8903f071ab73230c6
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-02-29 18:03:42 +0100

    MySQL script for lnxdiagd12

---
+++ table12.sql
@@ -0,0 +1,22 @@
+# MySQL script
+# create table for system CPU load
+# [ ] systemp = create table for system CPU temperatures
+# [x] sysload = create table for system CPU load
+# [ ] sysnetw = create table for system networkload
+# [ ] sysdskt = create table for system disk temperatures
+# [ ] sysmem  = create table for system memory usage
+
+USE domotica;
+
+DROP TABLE IF EXISTS sysload;
+
+CREATE TABLE `sysload` (
+  `sample_time`  datetime,
+  `sample_epoch` int(11) unsigned,
+  `host`         varchar(24),
+  `temperature`  decimal(6,3),
+  PRIMARY KEY (`sample_time`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
+
+# example to retrieve data:
+# mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM sysload where (sample_time) >=NOW() - INTERVAL 6 HOUR;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql.csv
