82d4e14da526b9bb421e56366928916a84a56c3a
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-04-04 19:14:18 +0200

    proper mysql command

--- graphday.sh
+++ graphday.sh
@@ -14,7 +14,7 @@ interval="INTERVAL 50 HOUR "
 host=$(hostname)
 
 pushd $HOME/lnxdiagd >/dev/null
-  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM systemp where (sample_time >=NOW() - $interval) AND (host = $host);" | sed 's/\t/;/g;s/\n//g' > /tmp/sql11.csv
+  mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM systemp where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > /tmp/sql11.csv
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM dht22  where (sample_time) >=NOW() - $interval;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql22.csv
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM bmp183 where (sample_time) >=NOW() - $interval;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql23.csv
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM ds18 where (sample_time) >=NOW() - $interval;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql24.csv
@@ -25,7 +25,7 @@ pushd $HOME/lnxdiagd >/dev/null
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql2c.csv
 
   #touch /tmp/bonediagd/graph.lock
-  #gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph22.gp
+  gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph11.gp
   #gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph23.gp
   #gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph24.gp
   #gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph25.gp
