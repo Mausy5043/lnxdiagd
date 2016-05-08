0f9b3ccd06deb22128e6a5df62539c81e159069a
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-08 09:59:38 +0200

    create the new graphs

--- graphs.sh
+++ graphs.sh
@@ -9,19 +9,19 @@ UTCSECONDS=$(date -d "$UTC" +%s)
 UTCOFFSET=$(($LOCALSECONDS-$UTCSECONDS))
 
 pushd $HOME/lnxdiagd >/dev/null
-  if [ $(cat /tmp/sql11.csv | wc -l) -gt 30 ]; then
+  if [ $(cat /tmp/sql11d.csv | wc -l) -gt 30 ]; then
     gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph11.gp
   fi
-  if [ $(cat /tmp/sql12.csv | wc -l) -gt 30 ]; then
+  if [ $(cat /tmp/sql12d.csv | wc -l) -gt 30 ]; then
     gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph12.gp
   fi
-  if [ $(cat /tmp/sql13.csv | wc -l) -gt 30 ]; then
+  if [ $(cat /tmp/sql13d.csv | wc -l) -gt 30 ]; then
     gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph13.gp
   fi
-  if [ $(cat /tmp/sql14.csv | wc -l) -gt 30 ]; then
+  if [ $(cat /tmp/sql14d.csv | wc -l) -gt 30 ]; then
     gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph14.gp
   fi
-  if [ $(cat /tmp/sql15.csv | wc -l) -gt 30 ]; then
+  if [ $(cat /tmp/sql15d.csv | wc -l) -gt 30 ]; then
     gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph15.gp
   fi
 
