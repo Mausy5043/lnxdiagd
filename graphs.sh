e2e31f4a222d20db43aebebce6650bee80ce7380
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-15 18:36:46 +0200

    thread carefully!

--- graphs.sh
+++ graphs.sh
@@ -10,19 +10,21 @@ UTCOFFSET=$((LOCALSECONDS - UTCSECONDS))
 
 pushd "$HOME/lnxdiagd" >/dev/null
   if [ $(wc -l < /tmp/lnxdiagd/mysql/sql11d.csv) -gt 30 ]; then
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph11.gp
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph11.gp &
   fi
   if [ $(wc -l < /tmp/lnxdiagd/mysql/sql12d.csv) -gt 30 ]; then
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph12.gp
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph12.gp &
   fi
   if [ $(wc -l < /tmp/lnxdiagd/mysql/sql13d.csv) -gt 30 ]; then
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph13.gp
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph13.gp &
   fi
   if [ $(wc -l < /tmp/lnxdiagd/mysql/sql14d.csv) -gt 30 ]; then
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph14.gp
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph14.gp &
   fi
   if [ $(wc -l < /tmp/lnxdiagd/mysql/sql15d.csv) -gt 30 ]; then
-    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph15.gp
+    gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph15.gp &
   fi
+
+wait
 
 popd >/dev/null
