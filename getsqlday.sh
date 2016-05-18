72d82c57e0e8fe005b6f8df526955d7296f9a9af
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-18 18:43:07 +0200

    Revert "mark log"
    This reverts commit a9c04718de2bb9af8c124e79242ffa2fe4fc6155.

--- getsqlday.sh
+++ getsqlday.sh
@@ -12,7 +12,6 @@ interval="INTERVAL 30 HOUR "
 host=$(hostname)
 
 pushd "$HOME/lnxdiagd" >/dev/null
-  logger -p 1 -t getsqlday "--MARK DAY--"
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM systemp where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql11d.csv"
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM sysload where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql12d.csv"
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM sysnet  where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql13d.csv"
