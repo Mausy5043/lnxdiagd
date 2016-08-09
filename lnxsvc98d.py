e4a41f53fe51185b52df236584a236dfd2d84505
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-08-09 12:14:14 +0200

    add timeout to lftp

--- lnxsvc98d.py
+++ lnxsvc98d.py
@@ -80,7 +80,7 @@ def do_mv_data(flock, homedir, script):
     write_lftp(script)
     cmnd = ['lftp', '-f', script]
     syslog_trace("...:  {0}".format(cmnd), False, DEBUG)
-    cmnd = subprocess.call(cmnd)
+    cmnd = subprocess.call(cmnd, timeout=20)
     syslog_trace("...:  {0}".format(cmnd), False, DEBUG)
 
 def getsqldata(homedir, nu):
