7129d23a28bbfaa76ad9119f905b2461a0576ba8
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-08-22 19:24:06 +0200

    table19 added

---
+++ table19.sql
@@ -0,0 +1,19 @@
+# MySQL script
+# create table for disk temperatures
+
+USE domotica;
+
+DROP TABLE IF EXISTS disktemp;
+
+CREATE TABLE `syslog` (
+  `sample_time`   datetime,
+  `sample_epoch`  bigint(20) unsigned,
+  `host`          varchar(24),
+  `diskid`        int(11) unsigned,
+  `diskt`         int(11) signed,
+  `id`            varchar(24),
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
+
+# example to retrieve data:
+# mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM syslog where (sample_time) >=NOW() - INTERVAL 6 HOUR;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql.csv
