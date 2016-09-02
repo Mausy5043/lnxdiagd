86ae4606dc98b9ee8d691d744453d8bdcda5f991
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-09-02 17:37:43 +0200

    add example of data

--- table19.sql
+++ table19.sql
@@ -17,3 +17,7 @@ CREATE TABLE `disktemp` (
 
 # example to retrieve data:
 # mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM syslog where (sample_time) >=NOW() - INTERVAL 6 HOUR;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql.csv
+
+#
+# 2016-09-02 17:19:01;1472829540;boson;wwn-0x50014ee605a043e2;39;wwn-0x50014ee605a043e2@1472829540
+# 2016-09-02 16:57:01;1472828220;boson;wwn-0x50026b723c0d6dd5;41;wwn-0x50026b723c0d6dd5@1472828220
