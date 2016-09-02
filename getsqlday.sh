f82102e98d9e39927c5d17a4577c661a0a6729c7
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-09-02 16:51:53 +0200

    add SQL queries for daemon19

--- getsqlday.sh
+++ getsqlday.sh
@@ -17,7 +17,10 @@ pushd "$HOME/lnxdiagd" >/dev/null
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM sysnet  where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql13d.csv"
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM sysmem  where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql14d.csv"
   mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM syslog  where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql15d.csv"
-
   #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql2c.csv
+
+  if [ "$host" == "boson" ]; then
+    mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM disktemp  where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql19d.csv"
+  fi
 popd >/dev/null
