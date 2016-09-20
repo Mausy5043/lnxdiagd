be36477a50f0a3929b6563975abcc1ed5ee221e1
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-09-20 17:49:53 +0200

    no need to include nodename

--- lnxsvc82d.py
+++ lnxsvc82d.py
@@ -104,7 +104,7 @@ def do_markdown(flock, fdata, hwdevice):
 
     # System ID
     f.write('!!! ')
-    f.write(uname[0] + ' ' + uname[1] + ' ' + uname[2] + ' ' + uname[3] + ' ' + uname[4] + ' ' + platform.platform() + '  \n')
+    f.write(uname[0] + ' ' + uname[2] + ' ' + uname[3] + ' ' + uname[4] + ' ' + platform.platform() + '  \n')
 
     # lnxdiagd branch
     f.write('!!! lnxdiagd   on: ' + lnxdiagdbranch + '\n\n')
