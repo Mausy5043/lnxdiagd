ace383408f12460ef3ff1b052c0110e035241610
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-09-25 08:59:10 +0200

    use new MySQL-scripts (*19*)

--- getsqlday.sh
+++ getsqlday.sh
@@ -21,6 +21,6 @@ pushd "$HOME/lnxdiagd" >/dev/null
   #mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql2c.csv
 
   if [ "$host" == "boson" ]; then
-    mysql -h sql.lan --skip-column-names -e "USE domotica; SELECT * FROM disktemp  where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "$datastore/sql19d.csv"
+    mysql -h sql.lan --skip-column-names  < data19d.sql | sed 's/\t/;/g;s/\n//g' > "$datastore/sql19d.csv"
   fi
 popd >/dev/null
