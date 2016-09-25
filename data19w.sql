db2a8b78abd4033bf5c1fc4da33f6adea2bb98fc
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-09-25 09:23:43 +0200

    sort the disk-ids same as physical ordering in the server.

--- data19w.sql
+++ data19w.sql
@@ -2,15 +2,15 @@ USE domotica;
 SELECT
   d1.sample_epoch,
   #    d1.diskid,
-  d1.diskt AS hda,
+  d1.diskt AS sdd,
   #    d2.diskid,
-  d2.diskt AS hdb,
+  d2.diskt AS hda,
   #	 d3.diskid,
-  d3.diskt AS hdc,
+  d3.diskt AS hdb,
   #    d4.diskid,
-  d4.diskt AS hdd,
+  d4.diskt AS hdc,
   #    d5.diskid,
-  d5.diskt AS sdd
+  d5.diskt AS hdd
 FROM
   disktemp d1,
   disktemp d2,
@@ -23,10 +23,10 @@ WHERE
 	AND d1.sample_epoch = d3.sample_epoch
 	AND d1.sample_epoch = d4.sample_epoch
 	AND d1.sample_epoch = d5.sample_epoch
-	AND (d1.diskid LIKE  '%20fce'
-		AND d2.diskid LIKE '%043e2'
-		AND d3.diskid LIKE '%a237b'
-		AND d4.diskid LIKE '%7b79c'
-		AND d5.diskid LIKE '%d6dd5'
+	  AND d1.diskid LIKE '%d6dd5'
+		AND d2.diskid LIKE '%20fce'
+		AND d3.diskid LIKE '%043e2'
+		AND d4.diskid LIKE '%a237b'
+		AND d5.diskid LIKE '%7b79c'
 		)
 ;
