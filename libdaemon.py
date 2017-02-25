b7923e5fc963e76a4308da1160fb25c416a40e75
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-02-25 16:24:01 +0100

    fix QC: consecutive if statements

--- libdaemon.py
+++ libdaemon.py
@@ -121,8 +121,7 @@ class Daemon:
         time.sleep(0.1)
 
     except OSError as err:
-      if err.errno == 3:
-        if os.path.exists(self.pidfile):
+      if (err.errno == 3) and os.path.exists(self.pidfile):
           os.remove(self.pidfile)
       else:
         print(str(err))
