20ecee2bb550a983fd641d3504a7a078c0da08bc
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-08-19 18:50:29 +0200

    show timings

--- queries/day.sh
+++ queries/day.sh
@@ -9,21 +9,21 @@ pushd "$HOME/lnxdiagd/queries/" >/dev/null || exit 1
 
   #sleep $(echo $RANDOM/555 |bc)
 
-  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM systemp where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql11d.csv"
-  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysload where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql12d.csv"
-  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysnet  where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql13d.csv"
-  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysmem  where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql14d.csv"
-  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM syslog  where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql15d.csv"
+  time mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM systemp where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql11d.csv"
+  time mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysload where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql12d.csv"
+  time mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysnet  where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql13d.csv"
+  time mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysmem  where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql14d.csv"
+  time mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM syslog  where (sample_time >=NOW() - ${D_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql15d.csv"
 
   #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
   #mysql -h sql --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - D_INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > /tmp/sql2c.csv
 
   if [ "${HOST}" == "boson" ]; then
-    mysql -h sql --skip-column-names  < data19d.sql | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql19d.csv"
+    time mysql -h sql --skip-column-names  < data19d.sql | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql19d.csv"
   fi
 
   # Get day data for system temperature (systemp; graph11)
-  mysql -h sql --skip-column-names -e             \
+  time mysql -h sql --skip-column-names -e             \
   "USE domotica;                                  \
    SELECT MIN(sample_time),                       \
           MIN(temperature),                       \
@@ -37,7 +37,7 @@ pushd "$HOME/lnxdiagd/queries/" >/dev/null || exit 1
   | sed 's/\t/;/g;s/\n//g' > "${DATASTORE4}/sql11d.csv"
 
   # Get day data for system load (sysload; graph12)
-  mysql -h sql --skip-column-names -e             \
+  time mysql -h sql --skip-column-names -e             \
   "USE domotica;                                  \
    SELECT MIN(sample_time),                       \
           AVG(load5min),                          \
