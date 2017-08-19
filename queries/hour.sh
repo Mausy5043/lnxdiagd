e71dca32ec2818516a191241d13f7e0be6f9efac
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-08-19 16:47:50 +0200

    move parameters to common include file

--- queries/hour.sh
+++ queries/hour.sh
@@ -2,56 +2,48 @@
 
 # Pull data from MySQL server and graph them.
 
-divider=60
-interval="INTERVAL 70 MINUTE "
-host=$(hostname)
-
-datastore="/tmp/lnxdiagd/mysql"
-if [ ! -d "${datastore}" ]; then
-  mkdir -p "${datastore}"
-fi
+# shellcheck disable=SC1091
+source ./sql-includes
 
 #sleep $(echo $RANDOM/555 |bc)
 
 pushd "$HOME/lnxdiagd" >/dev/null || exit 1
-  # mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM systemp where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql11h.csv"
-  # mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysload where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql12h.csv"
-  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysnet  where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql13h.csv"
-  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysmem  where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql14h.csv"
-  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM syslog  where (sample_time >=NOW() - $interval) AND (host = '$host');" | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql15h.csv"
+  # mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM systemp where (sample_time >=NOW() - ${H_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql11h.csv"
+  # mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysload where (sample_time >=NOW() - ${H_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql12h.csv"
+  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysnet  where (sample_time >=NOW() - ${H_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql13h.csv"
+  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM sysmem  where (sample_time >=NOW() - ${H_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql14h.csv"
+  mysql -h sql --skip-column-names -e "USE domotica; SELECT * FROM syslog  where (sample_time >=NOW() - ${H_INTERVAL}) AND (host = '${HOST}');" | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql15h.csv"
 
   #http://www.sitepoint.com/understanding-sql-joins-mysql-database/
-  #mysql -h sql --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > ${datastore}/sql2c.csv
-  if [ "$host" == "boson" ]; then
-    mysql -h sql --skip-column-names  < data19h.sql | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql19h.csv"
+  #mysql -h sql --skip-column-names -e "USE domotica; SELECT ds18.sample_time, ds18.sample_epoch, ds18.temperature, wind.speed FROM ds18 INNER JOIN wind ON ds18.sample_epoch = wind.sample_epoch WHERE (ds18.sample_time) >=NOW() - INTERVAL 1 MINUTE;" | sed 's/\t/;/g;s/\n//g' > ${DATASTORE}/sql2c.csv
+  if [ "${HOST}" == "boson" ]; then
+    mysql -h sql --skip-column-names  < data19h.sql | sed 's/\t/;/g;s/\n//g' > "${DATASTORE}/sql19h.csv"
   fi
 
-  datastore="/tmp/lnxdiagd/mysql4python"
-  if [ ! -d "${datastore}" ]; then
-    mkdir -p "${datastore}"
-  fi
 
   # Get hour data for system temperature (systemp; graph11)
-  mysql -h sql --skip-column-names -e \
-  "USE domotica;           \
-   SELECT MIN(sample_time), \
-          AVG(temperature)   \
-   FROM systemp               \
-   WHERE (sample_time >= NOW() - ${interval}) AND (host = '${host}') \
-   GROUP BY (sample_epoch DIV ${divider});" \
-  | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql11h.csv"
+  mysql -h sql --skip-column-names -e            \
+  "USE domotica;                                 \
+   SELECT MIN(sample_time),                      \
+          AVG(temperature)                       \
+    FROM systemp                                 \
+    WHERE (sample_time >= NOW() - ${H_INTERVAL}) \
+      AND (host = '${HOST}')                     \
+    GROUP BY (sample_epoch DIV ${H_DIVIDER});"   \
+  | sed 's/\t/;/g;s/\n//g' > "${DATASTORE4}/sql11h.csv"
 
   # Get hour data for system load (sysload; graph12)
-  mysql -h sql --skip-column-names -e \
-  "USE domotica; \
-   SELECT MIN(sample_time), \
-          AVG(load5min), \
-          AVG(user),  \
-          AVG(system), \
-          AVG(waiting)  \
-   FROM sysload \
-   WHERE (sample_time >= NOW() - ${interval}) AND (host = '${host}') \
-   GROUP BY (sample_epoch DIV ${divider});" \
-  | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql12h.csv"
+  mysql -h sql --skip-column-names -e            \
+  "USE domotica;                                 \
+   SELECT MIN(sample_time),                      \
+          AVG(load5min),                         \
+          AVG(user),                             \
+          AVG(system),                           \
+          AVG(waiting)                           \
+    FROM sysload                                 \
+    WHERE (sample_time >= NOW() - ${H_INTERVAL}) \
+      AND (host = '${HOST}')                     \
+    GROUP BY (sample_epoch DIV ${H_DIVIDER});"   \
+  | sed 's/\t/;/g;s/\n//g' > "${DATASTORE4}/sql12h.csv"
 
 popd >/dev/null
--- queries/year.sh
+++ queries/year.sh
@@ -2,39 +2,40 @@
 
 # Pull YEARLY data from MySQL server.
 
-######
+# shellcheck disable=SC1091
+source ./sql-includes
 
-interval="INTERVAL 370 DAY "
-host=$(hostname)
-
-datastore="/tmp/lnxdiagd/mysql4python"
-if [ ! -d "${datastore}" ]; then
-  mkdir -p "${datastore}"
-fi
+#sleep $(echo $RANDOM/555 |bc)
 
 pushd "$HOME/lnxdiagd" >/dev/null  || exit 1
   # Get year data for system temperature (systemp; graph11)
-  mysql -h sql --skip-column-names -e \
-  "USE domotica;            \
-   SELECT MIN(sample_time), \
-          MIN(temperature), \
-          AVG(temperature), \
-          MAX(temperature)  \
-   FROM systemp             \
-   WHERE (sample_time >= NOW() - ${interval}) AND (host = '${host}') \
-   GROUP BY YEAR(sample_time), MONTH(sample_time), WEEK(sample_time);" \
-  | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql11y.csv"
+  mysql -h sql --skip-column-names -e             \
+  "USE domotica;                                  \
+   SELECT MIN(sample_time),                       \
+          MIN(temperature),                       \
+          AVG(temperature),                       \
+          MAX(temperature)                        \
+    FROM systemp                                  \
+    WHERE (sample_time >= NOW() - ${Y_INTERVAL})  \
+      AND (host = '${HOST}')                      \
+    GROUP BY YEAR(sample_time),                   \
+             MONTH(sample_time),                  \
+             WEEK(sample_time);"                  \
+  | sed 's/\t/;/g;s/\n//g' > "${DATASTORE4}/sql11y.csv"
 
   # Get year data for system load (sysload; graph12)
-  mysql -h sql --skip-column-names -e \
-  "USE domotica; \
-   SELECT MIN(sample_time), \
-          AVG(load5min), \
-          AVG(user), \
-          AVG(system),  \
-          AVG(waiting) \
-   FROM sysload \
-   WHERE (sample_time >= NOW() - ${interval}) AND (host = '${host}') \
-   GROUP BY YEAR(sample_time), MONTH(sample_time), WEEK(sample_time);" \
-  | sed 's/\t/;/g;s/\n//g' > "${datastore}/sql12y.csv"
+  mysql -h sql --skip-column-names -e             \
+  "USE domotica;                                  \
+   SELECT MIN(sample_time),                       \
+          AVG(load5min),                          \
+          AVG(user),                              \
+          AVG(system),                            \
+          AVG(waiting)                            \
+    FROM sysload                                  \
+    WHERE (sample_time >= NOW() - ${Y_INTERVAL})  \
+      AND (host = '${HOST}')                      \
+    GROUP BY YEAR(sample_time),                   \
+             MONTH(sample_time),                  \
+             WEEK(sample_time);"                  \
+  | sed 's/\t/;/g;s/\n//g' > "${DATASTORE4}/sql12y.csv"
 popd >/dev/null
