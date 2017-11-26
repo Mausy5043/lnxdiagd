5b61967209b31dd5267d3ed380525c4c19a678ae
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-11-26 13:07:19 +0100

    timeout gnuplot commands as they tend to get stuck

--- mkgraphs.sh
+++ mkgraphs.sh
@@ -3,8 +3,10 @@
 # Pull data from MySQL server and graph them.
 
 AGE=0
+TIMEOUT=60
 if [[ $# -ne 0 ]]; then
   AGE=$1
+  TIMEOUT=$(echo "$AGE/2*60" | bc)
 fi
 
 LOCAL=$(date)
@@ -18,29 +20,29 @@ pushd "$HOME/lnxdiagd" >/dev/null  || exit 1
   if [[ $(find "/tmp//lnxdiagd/site/img/day12.png" -mmin +$AGE) || ! -f "/tmp//lnxdiagd/site/img/day12.png" ]]; then
     if [ "$(wc -l < /tmp/lnxdiagd/mysql4gnuplot/sql11d.csv)" -gt 5 ]; then
   		echo -n "Graph 11"
-      time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph11.gp
+      time timeout $TIMEOUT gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph11.gp
     fi
     if [ "$(wc -l < /tmp/lnxdiagd/mysql4gnuplot/sql12d.csv)" -gt 5 ]; then
   		echo -n "Graph 12"
-      time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph12.gp
+      time timeout $TIMEOUT gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph12.gp
     fi
     if [ "$(wc -l < /tmp/lnxdiagd/mysql4gnuplot/sql13d.csv)" -gt 5 ]; then
   		echo -n "Graph 13"
-      time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph13.gp
+      time timeout $TIMEOUT gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph13.gp
     fi
     if [ "$(wc -l < /tmp/lnxdiagd/mysql4gnuplot/sql14d.csv)" -gt 5 ]; then
   		echo -n "Graph 14"
-      time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph14.gp
+      time timeout $TIMEOUT gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph14.gp
     fi
     if [ "$(wc -l < /tmp/lnxdiagd/mysql4gnuplot/sql15d.csv)" -gt 5 ]; then
   		echo -n "Graph 15"
-      time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph15.gp
+      time timeout $TIMEOUT gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph15.gp
     fi
 
     if [ "${host}" == "boson" ]; then
       if [ "$(wc -l < /tmp/lnxdiagd/mysql4gnuplot/sql19d.csv)" -gt 5 ]; then
   		  echo -n "Graph 19"
-        time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph19.gp
+        time timeout $TIMEOUT gnuplot -e "utc_offset='${UTCOFFSET}'" ./graphs/graph19.gp
   		fi
     fi
   fi
