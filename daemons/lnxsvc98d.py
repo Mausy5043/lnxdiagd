197a7c5b5c8dede0afb5af49e3a87df897bb6a15
Mausy5043 <Mausy5043@users.noreply.github.com>
2018-05-05 16:04:13 +0200

    debug timers

--- daemons/lnxsvc98d.py
+++ daemons/lnxsvc98d.py
@@ -116,19 +116,19 @@ class SqlDataFetch(object):
     if t0 >= self.d_timer:
       self.d_dataisstale = self.get(self.d_cmd)
       t1 = time.time()
-      self.h_timer = t1 + self.d_updatetime
+      self.d_timer = t1 + self.d_updatetime
       # dt = t1 - t0  # determine query duration
       t0 = t1
     if t0 >= self.w_timer:
       self.w_dataisstale = self.get(self.w_cmd)
       t1 = time.time()
-      self.h_timer = t1 + self.w_updatetime
+      self.w_timer = t1 + self.w_updatetime
       # dt = t1 - t0  # determine query duration
       t0 = t1
     if t0 >= self.y_timer:
       self.y_dataisstale = self.get(self.y_cmd)
       t1 = time.time()
-      self.h_timer = t1 + self.y_updatetime
+      self.y_timer = t1 + self.y_updatetime
       # dt = t1 - t0  # determine query duration
     return time.time() - ts
 
