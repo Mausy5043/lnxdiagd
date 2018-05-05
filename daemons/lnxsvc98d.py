459d3415452e843686be455b6f4d5bc1ab5880b8
Mausy5043 <Mausy5043@users.noreply.github.com>
2018-05-05 15:45:43 +0200

    slight modifications

--- daemons/lnxsvc98d.py
+++ daemons/lnxsvc98d.py
@@ -23,9 +23,9 @@ IS_JOURNALD = os.path.isfile('/bin/journalctl')
 MYID        = "".join(list(filter(str.isdigit, os.path.realpath(__file__).split('/')[-1])))
 MYAPP       = os.path.realpath(__file__).split('/')[-3]
 NODE        = os.uname()[1]
-GRAPH_UPDATE      = 15   # in minutes
+GRAPH_UPDATE      = 10   # in minutes
 SQL_UPDATE_HOUR   = GRAPH_UPDATE  # in minutes (shouldn't be shorter than GRAPH_UPDATE)
-SQL_UPDATE_DAY    = 30  # in minutes
+SQL_UPDATE_DAY    = 27  # in minutes
 SQL_UPDATE_WEEK   = 4   # in hours
 SQL_UPDATE_YEAR   = 8   # in hours
 
