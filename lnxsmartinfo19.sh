6205572bc8efefabaecf8c9db239dd59544d88c3
Mausy5043 <Mausy5043@users.noreply.github.com>
2018-05-26 11:46:27 +0200

    don't need to know disk-id

--- lnxsmartinfo19.sh
+++ lnxsmartinfo19.sh
@@ -5,41 +5,44 @@
 # The resulting data is stored in `/tmp/mausy5043libs/*.dat` files that are
 # subsequently read by `daemons/lnxdiag19d.py`.
 
-rf="/tmp/mausy5043libs/smartinfo"
+TEMPDIR="/tmp/mausy5043libs"
+rf="${TEMPDIR}/smartinfo"
 
 # BEWARE
 # The disks identified here as `sda`, `sdb` etc. may not necessarily
 # be called `/dev/sda`, `/dev/sdb` etc. on the system!!
-sda="wwn-0x50026b723c0d6dd5"  # SSD 50026B723C0D6DD5
-sdb="wwn-0x50014ee261020fce"  # WD-WCC4N5PF96KD
-sdc="wwn-0x50014ee605a043e2"  # WD-WMC4N0K01249
-sdd="wwn-0x50014ee6055a237b"  # WD-WMC4N0J6Y6LW
-sde="wwn-0x50014ee60507b79c"  # WD-WMC4N0E24DVU
-sdf="wwn-0x50014ee262ed6df5"  # WD-WCC4J0JPYS0D
+#sda="wwn-0x50026b723c0d6dd5"  # SSD 50026B723C0D6DD5
+#sdb="wwn-0x50014ee261020fce"  # WD-WCC4N5PF96KD
+#sdc="wwn-0x50014ee605a043e2"  # WD-WMC4N0K01249
+#sdd="wwn-0x50014ee6055a237b"  # WD-WMC4N0J6Y6LW
+#sde="wwn-0x50014ee60507b79c"  # WD-WMC4N0E24DVU
+#sdf="wwn-0x50014ee262ed6df5"  # WD-WCC4J0JPYS0D
 # sdgl=""
 
 function smart1999 {
-  if [[ -e "/dev/disk/by-id/${1}" ]]; then
-    if [[ ! -e "${rf}-${1}-i.dat" ]]; then
+  # p is the path into the /dev tree
+  p="${1}"
+  # b is the device id
+  b=basename "${p}"
+  if [[ -e "${p}" ]]; then
+    if [[ ! -e "${rf}-${b}-i.dat" ]]; then
       # this is static info, therefore only get it if it's not there.
-      smartctl -i "/dev/disk/by-id/${1}" |awk 'NR>4' > "${rf}-${1}-i.dat"
+      smartctl -i "${p}" |awk 'NR>4' > "${rf}-${b}-i.dat"
     fi
-    smartctl -A "/dev/disk/by-id/${1}" |awk 'NR>7' > "${rf}-${1}-A.dat"
-    smartctl -H "/dev/disk/by-id/${1}" |grep 'test result' > "${rf}-${1}-H.dat"
-    smartctl -l selftest "/dev/disk/by-id/${1}" |grep '\# 1' > "${rf}-${1}-l.dat"
+    smartctl -A "${p}" |awk 'NR>7' > "${rf}-${b}-A.dat"
+    smartctl -H "${p}" |grep 'test result' > "${rf}-${b}-H.dat"
+    smartctl -l selftest "${p}" |grep '\# 1' > "${rf}-${b}-l.dat"
     chmod 744 "${rf}-*"
   fi
 }
 
-if [[ ! -d /tmp/mausy5043libs ]]; then
-  mkdir -m 777 /tmp/mausy5043libs
+if [[ ! -d "${TEMPDIR}" ]]; then
+  mkdir -m 777 "$TEMPDIR"
 fi
 
 touch "${rf}.lock"
-smart1999 $sda
-smart1999 $sdb
-smart1999 $sdc
-smart1999 $sdd
-smart1999 $sde
-smart1999 $sdf
+find /dev/disk/by-id -name "wwn*" |grep -v "part" > "${TEMPDIR}/smartdisks.txt"
+while read p; do
+  smart1999 "${p}"
+done <"${TEMPDIR}/smartdisks.txt"
 rm "${rf}.lock"
